a12506a1edd26aebea9a912a0986e19527ef3304
SlawomirR <no.for.spam@wp.pl>
2018-09-09 11:11:16 +0200

    Initial commit

---
+++ src/main/java/io/github/slawomirr/taskswithangularjs/TasksWithAngularjsApplication.java
@@ -0,0 +1,12 @@
+package io.github.slawomirr.taskswithangularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksWithAngularjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksWithAngularjsApplication.class, args);
+    }
+}
