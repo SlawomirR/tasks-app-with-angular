a0ce3a7282a61aa9d752f37eed8f8f633c705a66
SlawomirR <no.for.spam@wp.pl>
2018-09-09 16:42:47 +0200

    Added components. Closes #5

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {TasksComponent} from './tasks/tasks.component';
+import {TasksListComponent} from './tasks/tasks-list/tasks-list.component';
+import {TasksAddComponent} from './tasks/tasks-add/tasks-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TasksListComponent,
+    TasksAddComponent
   ],
   imports: [
     BrowserModule
---
+++ src/main/frontend/src/app/tasks/tasks-add/tasks-add.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-add',
+  templateUrl: './tasks-add.component.html',
+  styleUrls: ['./tasks-add.component.css']
+})
+export class TasksAddComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
